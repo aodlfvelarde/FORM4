b5646423c2259ba96849ab5584b09992d0080940
aodlfvelarde <129921638+aodlfvelarde@users.noreply.github.com>
2024-08-15 14:01:12 -0400

    version1

---
+++ javascripts/sample.js
@@ -0,0 +1,15 @@
+function addLink() {
+    const element = document.createElement("a");
+    element.setAttribute("href", "https://diveintocode.jp/");
+    element.innerText = "DPro";
+    const parentElement = document.getElementsByTagName("div")[0];
+    parentElement.appendChild(element);
+};
+function removeLink() {
+    const link = document.getElementsByTagName("a")[0];
+    link.removeAttribute("href");
+}
+window.onload = function() {
+    addLink();
+    removeLink();
+}
